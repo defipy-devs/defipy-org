08c0f1cd763516fbdfb1a475d551738ef41bec24
icmoore <ic3moore@gmail.com>
2026-04-26 23:16:00 -0700

    Switch GA4 to standalone defipy.org property (G-TVT5JQT6WV)

--- astro.config.mjs
+++ astro.config.mjs
@@ -35,12 +35,12 @@ export default defineConfig({
 					tag: 'script',
 					attrs: {
 						async: true,
-						src: 'https://www.googletagmanager.com/gtag/js?id=G-CHT6JCJ9ET',
+						src: 'https://www.googletagmanager.com/gtag/js?id=G-TVT5JQT6WV',
 					},
 				},
 				{
 					tag: 'script',
-					content: `window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-CHT6JCJ9ET');`,
+					content: `window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-TVT5JQT6WV');`,
 				},
 			],
 			// Sidebar default-state policy:
